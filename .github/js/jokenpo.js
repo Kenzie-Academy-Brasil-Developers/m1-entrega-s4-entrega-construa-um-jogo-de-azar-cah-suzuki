edb242f61ff153c13f0c3c9afd32fe418cf08316
camila suzuki <camila.suzuki.p@gmail.com>
2021-11-24 14:22:45 -0300

    jokenpo logica de mostrar card jogador

--- .github/js/jokenpo.js
+++ .github/js/jokenpo.js
@@ -9,22 +9,22 @@
 
 
 
-const inputUserName = document.querySelector("#nameUser");
-const inputLink1 = document.querySelector("#link1");
+// const inputUserName = document.querySelector("#nameUser");
+// const inputLink1 = document.querySelector("#link1");
 
-let inputLink1Value = inputLink1.href
+// let inputLink1Value = inputLink1.href
 
-function handleChange(){
-    inputLink1.href = inputLink1Value + "?name=" + inputUserName.value
-    console.log(inputLink1.href)
-}
+// function handleChange(){
+//     inputLink1.href = inputLink1Value + "?name=" + inputUserName.value
+//     console.log(inputLink1.href)
+// }
 
-inputUserName.addEventListener('change', handleChange);
+// inputUserName.addEventListener('change', handleChange);
 
-const cardUm = document.querySelector("#cardUm")
-cardUm.addEventListener("click",(e)=>{
-    cardUm.classList.toggle("flip")
-})
+// const cardUm = document.querySelector("#cardUm")
+// cardUm.addEventListener("click",(e)=>{
+//     cardUm.classList.toggle("flip")
+// })
 
 
 
