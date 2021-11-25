5429e779a482259cbe07b978f2d4e86b90ca3488
camila suzuki <camila.suzuki.p@gmail.com>
2021-11-25 12:04:57 -0300

    layout final

--- .github/js/jokenpo.js
+++ .github/js/jokenpo.js
@@ -1,34 +1,14 @@
 
-// Localiza os parametros passados na URL e já me retorna o equivalente ao name 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-let userName = urlParams.get("name")
-console.log(userName)
-
 const outputNomeJogador = document.getElementById("nome-jogador")
-outputNomeJogador.innerHTML=userName
 const outputNomeJogadorModal = document.getElementById("nome-jogador-modal")
+
+
+let userName = urlParams.get("name")
+outputNomeJogador.innerHTML=userName
 outputNomeJogadorModal.innerHTML=userName
 
-
-
-
-// function numeroAleatorio(min,max){
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-
-// console.log(numeroAleatorio(1,10))
-
-
-
-
-
-// const cardUm = document.querySelector("#cardUm")
-// cardUm.addEventListener("click",(e)=>{
-//     cardUm.classList.toggle("flip")
-// })
 
 
 
